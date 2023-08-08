8acc3cc58ddfbe825dce87ef1ec17acf389cad83
Michael Neves Silva <108095311+Minesi@users.noreply.github.com>
2023-08-08 20:00:50 -0300

    Add files via upload
    Inicio das aulas de Lógica de Programação

---
+++ LógicaDeProgramação/Aula1/conceitos.js
@@ -0,0 +1,19 @@
+// Armazenar Dados
+// chave = "valor";
+escola = "Ada";
+
+// Executar Instruções (Tarefas)
+//instrucao();
+prompt();
+
+// intrucao(parametro);
+prompt("Digite seu nome:");
+
+// intrucao(param1, param2);
+prompt("Digite seu nome", "Seu nome aqui");
+
+/*
+contexto.instrucao();
+contexto.instrucao(param1);
+contexto.instrucao(param1, param2);
+*/
